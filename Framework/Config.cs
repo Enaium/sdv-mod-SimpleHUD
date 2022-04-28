d60681fd8990e999b25f9f650aaf88e1d9a19d58
Enaium <wyy-666@hotmail.com>
2022-04-28 18:37:53 +0800

    first commit

---
+++ Framework/Config.cs
@@ -0,0 +1,14 @@
+﻿using EnaiumToolKit.Framework.Utils;
+using StardewModdingAPI;
+
+namespace SimpleHUD.Framework
+{
+    public class Config
+    {
+        public SButton OpenSetting { get; set; } = SButton.H;
+        public bool Enable { get; set; } = true;
+        public string Title { get; set; } = "Simple HUD";
+        
+        public ColorUtils.NameType TextColor = ColorUtils.NameType.Aqua;
+    }
+}
